2fb983bde7a840531da3bfd68b33e2bdd77ceab8
sreecharan-desu <sreecharan309@gmail.com>
2024-09-15 19:36:42 +0530

    updated urls

--- frontend/src/components/signin-component.tsx
+++ frontend/src/components/signin-component.tsx
@@ -32,7 +32,7 @@ export function Signin({ type }: SigninProps) {
             return;
         }
         const bodyData = JSON.stringify({ username, password });
-        const res = await fetch(`http://localhost:3000/api/v1/${type}/signin`, {
+        const res = await fetch(`https://uni-z-api.vercel.app/api/v1/${type}/signin`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
