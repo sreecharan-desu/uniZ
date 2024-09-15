3ac719ed3d3020f68e63384fbff3652553d3dbbc
sreecharan-desu <sreecharan309@gmail.com>
2024-09-15 20:08:18 +0530

    updated urls

--- frontend/src/apis.ts
+++ frontend/src/apis.ts
@@ -14,16 +14,16 @@
 
 
 
-export const GET_OUTING_REQUESTS = 'https://uni-z-api.vercel.app/admin/getoutingrequests';
-export const GET_OUTPASS_REQUESTS = 'https://uni-z-api.vercel.app/admin/getoutpassrequests'
-export const STUDENT_OUTSIDE_CAMPUS = 'https://uni-z-api.vercel.app/admin/getstudentsoutsidecampus'
-export const STUDENT_INFO = "https://uni-z-api.vercel.app/student/getdetails";
-export const APPROVE_OUTING = "https://uni-z-api.vercel.app/admin/approveouting";
-export const REJECT_OUTING = "https://uni-z-api.vercel.app/admin/rejectouting";
-export const APPROVE_OUTPASS = "https://uni-z-api.vercel.app/admin/approveoutpass"
-export const REJECT_OUTPASS = "https://uni-z-api.vercel.app/admin/rejectoutpass"
-export const REQUEST_OUTING = "https://uni-z-api.vercel.app/student/requestouting"
-export const REQUEST_OUTPASS = "https://uni-z-api.vercel.app/student/requestoutpass"
-export const RESET_PASS = "https://uni-z-api.vercel.app/student/resetpass"
-export const SEARCH_STUDENTS = "https://uni-z-api.vercel.app/admin/searchstudent";
-export const UPDATE_STUDENT_STATUS = "https://uni-z-api.vercel.app/admin/updatestudentstatus";
+export const GET_OUTING_REQUESTS = 'https://uni-z-api.vercel.app/api/v1/admin/getoutingrequests';
+export const GET_OUTPASS_REQUESTS = 'https://uni-z-api.vercel.app/api/v1/admin/getoutpassrequests'
+export const STUDENT_OUTSIDE_CAMPUS = 'https://uni-z-api.vercel.app/api/v1/admin/getstudentsoutsidecampus'
+export const STUDENT_INFO = "https://uni-z-api.vercel.app/api/v1/student/getdetails";
+export const APPROVE_OUTING = "https://uni-z-api.vercel.app/api/v1/admin/approveouting";
+export const REJECT_OUTING = "https://uni-z-api.vercel.app/api/v1/admin/rejectouting";
+export const APPROVE_OUTPASS = "https://uni-z-api.vercel.app/api/v1/admin/approveoutpass"
+export const REJECT_OUTPASS = "https://uni-z-api.vercel.app/api/v1/admin/rejectoutpass"
+export const REQUEST_OUTING = "https://uni-z-api.vercel.app/api/v1/student/requestouting"
+export const REQUEST_OUTPASS = "https://uni-z-api.vercel.app/api/v1/student/requestoutpass"
+export const RESET_PASS = "https://uni-z-api.vercel.app/api/v1/student/resetpass"
+export const SEARCH_STUDENTS = "https://uni-z-api.vercel.app/api/v1/admin/searchstudent";
+export const UPDATE_STUDENT_STATUS = "https://uni-z-api.vercel.app/api/v1/admin/updatestudentstatus";
