04e2a7d850de2bae19eb4b4758bedbe0a4eb7ea9
sreecharan-desu <sreecharan309@gmail.com>
2024-09-15 17:38:32 +0530

    added vercel.json

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { mainRoute } from "./routes";const app = express();
+import { mainRoute } from "./routes";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
 
 // Middleware
 app.use(express.json());
@@ -13,7 +16,10 @@ app.get('/',(req,res) =>{
 })
 
 
+// Define port with a fallback to 3000
+const port = process.env.PORT || 3000;
+
 // Start the server
-app.listen(3000, () => {
-  console.log(`Listening on Port number ${3000}`);
+app.listen(port, () => {
+  console.log(`Listening on Port number ${port}`);
 });
