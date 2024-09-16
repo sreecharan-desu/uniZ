cb24e7bb86c269d7154959ef254a59ac61b79928
sreecharan-desu <sreecharan309@gmail.com>
2024-09-16 09:10:52 +0530

    modification search

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { studentRouter } from "./student/student";
 import { adminRouter } from "./admin/admin";
+import { updateDB } from "./helper-functions";
 // import { addAdmin } from "./helper-functions";
+
+updateDB();
 
 export const mainRoute = express.Router();
 // addAdmin('admin','1234567890');
