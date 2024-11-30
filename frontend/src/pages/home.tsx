fad4a91ad479c137ae87b232debfe95d0990a18b
sreecharan-desu <sreecharan309@gmail.com>
2024-11-30 18:41:01 +0530

    added vercel analytics

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,11 +1,15 @@
 import { useIsAuth } from "../customhooks/is_authenticated";
 import '../index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 export function Home() {
     useIsAuth();    
     return (
-        <div className="mx-5 text-center flex flex-col justify-center">
-            <p className="text-6xl font-semibold mb-6">Welcome to uniZ</p>
-        </div>
+        <>
+            <Analytics/>
+            <div className="mx-5 text-center flex flex-col justify-center">
+                <p className="text-6xl font-semibold mb-6">Welcome to uniZ</p>
+            </div>
+        </>
     );
 }
