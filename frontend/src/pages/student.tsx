f1a79afa82c37bff48795b15b6463b7ee4ed3503
sreecharan-desu <sreecharan309@gmail.com>
2024-12-01 11:38:23 +0530

    changed welcome message

--- frontend/src/pages/student.tsx
+++ frontend/src/pages/student.tsx
@@ -1,17 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useRecoilValue } from "recoil";
 import { useIsAuth } from "../customhooks/is_authenticated";
 import { useStudentData } from "../customhooks/student_info";
+import { student } from "../store";
 export function Student() {
     useIsAuth();
     useStudentData();
+    const username = useRecoilValue(student);
     return (
         <>
-        <p className="text-center font-bold">
-
-            Hi there
-
+        <p className="text-center font-bold first-letter:text-2xl">
+            Welcome back , {username.name.toString()} <br />
+            Seems like you've got some work here! Let's make it done together.
         </p>
-            {/* <Sidebar content="dashboard"/> */}
         </>
     );
 }
