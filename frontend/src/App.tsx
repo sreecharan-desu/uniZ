4371aab9dcf990bea221dd26f226059ba72d2bc4
SreeCharan <sreecharan309@gmail.com>
2025-02-18 12:35:59 +0530

    FEAT (maintianence) : website under maintainence

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -79,7 +79,7 @@ function Maintenance() {
     <div className="flex w-full rounded-md flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800">🚧 Under Maintenance 🚧</h1>
       <p className="text-lg text-gray-600 mt-2">
-        SreeCharan is currently working on some improvements. Check back soon! - <a href='https://sr3x0r.vercel.app' className='text-decoration-none text-blue-400' >Sr3X0r</a>
+        I'm is currently working on some improvements. Check back soon! - <a href='https://sr3x0r.vercel.app' className='text-decoration-none text-blue-400' >Sr3X0r</a>
       </p>
     </div>
   );
