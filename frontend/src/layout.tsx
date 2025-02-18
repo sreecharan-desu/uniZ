36ad74c8dac0b104675be039e0cc8391a1bf818d
SreeCharan <sreecharan309@gmail.com>
2025-02-18 09:28:30 +0530

    FEAT : nice loading anim

--- frontend/src/layout.tsx
+++ frontend/src/layout.tsx
@@ -10,11 +10,17 @@ interface LayoutProps {
 export function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen flex flex-col">
-      <Suspense fallback={<div className="text-center py-4">Loading Navbar...</div>}>
+      <Suspense fallback={<LoadingAnim/>}>
         <Navbar />
       </Suspense>
       <main className="flex-grow m-10">{children}</main>
       <Footer/>
     </div>
   );
+}
+
+const LoadingAnim = () => {
+  return <div className="flex items-center justify-center h-screen">
+    <div className="w-10 h-10 border-4 border-gray-800 border-t-white rounded-full animate-spin"></div>
+  </div>
 }
