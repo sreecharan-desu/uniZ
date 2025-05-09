5d78c599eba974c22ef5629440b28774422b31df
SreeCharan Desu <sreecharan309@gmail.com>
2025-05-09 20:19:14 +0530

    Completed version-1

--- frontend/src/pages/GradeHub.tsx
+++ frontend/src/pages/GradeHub.tsx
@@ -1,11 +1,8 @@
 export default function GradeHub() {
-
     return (
-        <div className='flex flex-row items-center justify-center h-screen w-96 mx-auto'>
-            
-            <p>GradeHub</p>
-
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-4">GradeHub</h1>
+        <p className="text-lg">GradeHub page content goes here.</p>
         </div>
-        
     );
-}
+    }
