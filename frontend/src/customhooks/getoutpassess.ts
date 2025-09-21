0850b2ce14f5175f64d6e2f983ea0c061ea7b4bd
sreecharan-desu <sreecharan309@gmail.com>
2025-09-21 12:29:13 +0530

    same as previous commit

---
+++ frontend/src/customhooks/getoutpassess.ts
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+import { outpasses } from "../store";
+import { GET_OUTPASS_REQUESTS } from "../apis";
+
+export function useGetOutpasses(){
+    const setOutpasses = useSetRecoilState(outpasses);
+    useEffect(()=>{
+        const token = localStorage.getItem('admin_token');
+        if(token){
+            const getDetails = async()=>{
+                const res = await fetch(GET_OUTPASS_REQUESTS,{
+                    method : 'GET',
+                    headers : {
+                        'Content-Type' : 'application/json',
+                        'Authorization' : `Bearer ${JSON.parse(token)}`
+                    },
+                });
+                const data = await res.json();
+                setOutpasses(data.outpasses)
+            }
+            getDetails();
+            setInterval(()=>getDetails(),60*1000)  
+        }
+    },[])
+}
