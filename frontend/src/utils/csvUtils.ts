32f850f5cc91e19375326ccf1bf006b4686ccc7e
sreecharan-desu <sreecharan309@gmail.com>
2025-10-02 12:37:31 +0530

    Add csvUtils helper

---
+++ frontend/src/utils/csvUtils.ts
@@ -0,0 +1,13 @@
+// utils/csvUtils.ts
+import Papa from "papaparse";
+
+export const parseCSV = <T>(file: File): Promise<T[]> => {
+  return new Promise((resolve, reject) => {
+    Papa.parse<T>(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: (results) => resolve(results.data),
+      error: (err) => reject(err),
+    });
+  });
+};
